35f66a069cdc4543519e9a119b939a7a42f2d08a
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2021-09-27 18:06:40 -0300

    iniciando server com rotas

--- streams.js
+++ streams.js
@@ -1,7 +1,17 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream('./docs/blog1.txt', {enconding: 'uft8'});
+const writeStream = fs.createWriteStream('./blog3.txt')
 
+
+// aqui vai ler de um arquivo e passar pro outro
 readStream.on('data', (chunk) => {
     console.log('------- NEW CHUNK -------')
+    writeStream.write('\nNew Chunk\n');
+    writeStream.write(chunk);
 });
+
+// piping
+
+// esse código faz a mesma coisa que o anterior, mas em apenas uma linha (mais simples)
+readStream.pipe(writeStream)
