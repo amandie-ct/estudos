5719b3a4d2b985b95351c45f6d195b6fe7cbb6a5
Amanda Carvalho <carvalhotp.amanda@gmail.com>
2021-09-28 19:27:31 -0300

    view engine e algumas middlewares adicionadas

--- app.js
+++ app.js
@@ -3,23 +3,49 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
+// headers (exemplo de middleware)
+// app.use((req, res, next) => {
+//     console.log('new request made');
+//     console.log('host', req.hostname);
+//     console.log('path', req.path);
+//     console.log('method', req.method);
+//     next();
+// });
+
+
+// middleware & static files
+// css, images, etc
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname});
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+        {title: 'Mario finds start', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+        {title: 'How to defeat Bowser', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
+    ]
+    res.render('index', {title: 'Home', blogs: blogs});
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', {title: 'About'});
 });
 
-// redirecting
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create'});
+})
 
 // 404 - tem que ficar no final do código senão vai impedir o "match" das outras páginas
-app.use('/404', (req, res) => {
-    res..status(404).sendFile('./views/404.html', {root: __dirname});
+app.use((req, res) => {
+    res.status(404).render('404', {title: '404'});
 })
+
+// redirecting
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about');
+// });
